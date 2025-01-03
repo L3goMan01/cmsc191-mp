7ba0c7b5560a9bacb27f10a25fb82ebb39b89083
sakurachu06 <anheresuribera@gmail.com>
2025-01-03 17:21:28 +0800

    view file modal

--- cmsc191-mp/app/pgh_dps/view_prescriptions/page.js
+++ cmsc191-mp/app/pgh_dps/view_prescriptions/page.js
@@ -52,7 +52,7 @@ export default function ViewPrescriptions() {
                         {/* Modal header */}
                         <div className="flex items-start justify-between p-4 border-b rounded-t">
                             <h3 className="text-xl font-semibold text-gray-900">
-                                Prescription
+                                Edit Prescription
                             </h3>
                             <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="editPrescriptionModal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -141,12 +141,45 @@ export default function ViewPrescriptions() {
 
                         {/* Modal body */}
                         <div className="p-6 space-y-6">
+                            <div className="grid grid-cols-2 gap-6">
+                                <div className="relative">
+                                    <label htmlFor="patient-name" className="block mb-2 text-sm font-medium text-gray-900">Patient Name</label>
+                                    <input type="text" name="patient-name" id="patient-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" placeholder="Jane Doe" readOnly />
+                                </div>
+                                <div className="relative">
+                                    <label htmlFor="prescribed-med" className="block mb-3.5 text-sm font-medium text-gray-900">Prescribed Medication</label>
+                                    <p className="text-gray-600">123456789</p>
+                                </div>
+                            </div>
+
+                            <div className="grid grid-cols-5 gap-6">
+                                <div className="relative">
+                                    <label htmlFor="generic-name" className="block mb-2 text-sm font-medium text-gray-900">Generic Name</label>
+                                    <input type="text" name="generic-name" id="generic-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" placeholder="Generika" readOnly />
+                                </div>
+                                <div className="relative">
+                                    <label htmlFor="brand-name" className="block mb-2 text-sm font-medium text-gray-900">Brand Name</label>
+                                    <input type="text" name="brand-name" id="brand-name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" placeholder="Bear Brand" readOnly />
+                                </div>
+                                <div className="relative">
+                                    <label htmlFor="amount" className="block mb-2 text-sm font-medium text-gray-900">Amount</label>
+                                    <input type="number" name="amount" id="amount" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" placeholder="1" readOnly />
+                                </div>
+                                <div className="relative col-span-2">
+                                    <label htmlFor="instructions" className="block mb-2 text-sm font-medium text-gray-900">Instructions</label>
+                                    <input type="text" name="instructions" id="instructions" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" placeholder="Once a day" readOnly />
+                                </div>
+                                <div className="relative col-span-5">
+                                    <label htmlFor="notes" className="block mb-2 text-sm font-medium text-gray-900">Notes</label>
+                                    <textarea name="notes" id="notes" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5" wrap="hard" rows={5} cols={10} placeholder="Waga waga" readOnly />
+                                </div>
+                            </div>
 
                         </div>
                         {/* Modal footer */}
-                        <div className="flex items-center p-6 space-x-3 rtl:space-x-reverse border-t border-gray-200 rounded-b">
+                        {/* <div className="flex items-center p-6 space-x-3 rtl:space-x-reverse border-t border-gray-200 rounded-b">
                             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save all</button>
-                        </div>
+                        </div> */}
                     </form>
                 </div>
             </div>
